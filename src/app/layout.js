3410b6cc103ce5e8572cddb76a9f52f978a2b5c5
GOLF2546 <Pattarapolnanakorn@gmail.com>
2023-08-07 15:43:00 +0700

    Finish

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Pattarapol Na Nakorn"
+            studentId="650610796"
           />
         </div>
       </body>
